8fc0e17c9c273b1e0584e83a60fbb6191b5283c7
Adriano Oliveira | 2BX <140265935+adriano2bx@users.noreply.github.com>
2025-08-13 15:06:51 -0300

    Create worker.py

---
+++ worker.py
@@ -0,0 +1,14 @@
+# worker.py
+import os
+import redis
+from rq import Worker, Queue, Connection
+
+listen = ['default']
+redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
+conn = redis.from_url(redis_url)
+
+if __name__ == '__main__':
+    with Connection(conn):
+        print("Iniciando Worker do Redis...")
+        worker = Worker(map(Queue, listen))
+        worker.work()
