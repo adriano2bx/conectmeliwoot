56cc285f66c5e7b707f292965233d07254fa7f03
Adriano Oliveira | 2BX <140265935+adriano2bx@users.noreply.github.com>
2025-08-13 16:26:04 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,12 @@ if [ "$APP_MODE" = "SETUP" ]; then
   exec python setup_app.py
 else
   echo ">>> Running in RUN mode..."
+  
+  # --- PASSO DE DIAGNÓSTICO ---
+  echo "--- Verificando o conteúdo de /etc/supervisor/conf.d/ ---"
+  ls -la /etc/supervisor/conf.d/
+  echo "--------------------------------------------------------"
+  
+  # Inicia a aplicação principal
   exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
 fi
